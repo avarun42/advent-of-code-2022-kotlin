1535039b7ef9f01cb9272ebdbb464e148f22fc0b
Varun Arora <varun@partyround.com>
2022-12-02 10:25:08 -0500

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,26 @@
 fun main() {
+    fun calculateCalorieSums(input: List<String>): List<Int> {
+        return input.fold(listOf(0)) { acc, s ->
+            if (s.isEmpty()) {
+                acc + 0
+            } else {
+                acc.dropLast(1) + (acc.last() + s.toInt())
+            }
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return calculateCalorieSums(input).max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return calculateCalorieSums(input).sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
